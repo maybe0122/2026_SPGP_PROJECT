c486e6cf97f3114ad6f44af5825f3b024d09ae87
Kim Kyu Min <kyuminkim0122@gmail.com>
2026-05-04 13:49:17 +0900

    update activity
    update mainactivity to horizontal
    add helltakeractivity

---
+++ HelltakerMobile/app/src/main/java/kr/ac/tukorea/ge/spgp/kyumin/helltakermobile/HelltakerMobileActivity.kt
@@ -0,0 +1,13 @@
+package kr.ac.tukorea.ge.spgp.kyumin.helltakermobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HelltakerMobileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
